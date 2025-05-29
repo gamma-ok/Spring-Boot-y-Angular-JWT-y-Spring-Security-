40722f526a55860422ea82bc32d41c48d7310a4f
gamma <santiagogamarrasanchez@gmail.com>
2025-05-29 09:54:45 -0500

    Primer commit

---
+++ src/main/java/pe/com/sistema/gamma/SistemaGammaBackedApplication.java
@@ -0,0 +1,13 @@
+package pe.com.sistema.gamma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGammaBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGammaBackedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/sistema/gamma/SistemaGammaBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.sistema.gamma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGammaBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
